d38960a3750bf90aa045e54426c293b7f8ba78d2
JN107154 <jn107154@dell.corp.int>
2018-03-02 13:55:20 -0800

    adding gitignore

---
+++ example.py
@@ -0,0 +1,4 @@
+import xmlparser as xp
+
+
+
--- xmlparser.py
+++ xmlparser.py
@@ -25,6 +25,29 @@ def getXML(url):
     xmlresult = ElementTree.fromstring(result.text)
     return xmlresult
 
+def showXMLTree(xmlobject, deep = 3):
+    for child in xmlobject:
+        print(child.tag)
+        if deep > 1:
+            for subchild in child:
+                print("--", subchild.tag)
+                if deep >2:
+                    for subsub in subchild:
+                        print('  --', subsub.tag)
+    return None
+
+
+
+class CaseViewer():
+    
+    def __init__(self, xmlobject):
+        self.xmlobjec = xmlobject
+    
+    def get_vehicles(self):
+        CaseForm = self.xmlobject.find('CaseForm')
+        Vehicles = CaseForm.find('Vehicles')
+        return Vehicles
+
 
 
 
