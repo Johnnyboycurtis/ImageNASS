97aa9fbb318389d71f340e1064d4d5c51b163864
JN107154 <jn107154@dell.corp.int>
2018-03-06 09:21:39 -0800

    fixing urls and directory paths

--- pynass/imagerequests.py
+++ pynass/imagerequests.py
@@ -9,33 +9,41 @@ from tqdm import tqdm
 
 
 class NASSImageRequest():
+    
     def __init__(self, CaseID, directory):
         """
         Builds URLs to images in NASS database and downloads images to specified directory
         """
-        if isinstance(CaseID, list) or isinstance(CaseIDs, tuple):
-            CaseID = [CaseID]
         self.CaseID = CaseID
         self.directory = directory
         self.URL = self.generate_URL()
     
-    def generate_URL():
+    def generate_URL(self):
         "Generate URLs for NASS image scraper"
-        URL = []
+        URL = dict()
         for caseid in self.CaseID:
-            url_full = "https://crashviewer.nhtsa.dot.gov/nass-sci/CaseForm.aspx?ViewText&CaseID={}&xsl=textonly.xsl&websrc=true".format(caseid)
-            URL.append(url_full)
+            url_full = "https://crashviewer.nhtsa.dot.gov/nass-cds/CaseForm.aspx?ViewText&CaseID={}&xsl=textonly.xsl&websrc=true".format(caseid)
+            URL[caseid] = url_full
         return URL
 
-    def pull_images(progress_bar=True):
-        URL = self.URL
+    def pull_images(self, progress_bar=True):
+        url_part1 = 'https://crashviewer.nhtsa.dot.gov/nass-cds/'
+        dir_name = self.directory
+        URL = self.URL.items()
         if progress_bar:
-            URL = tqdm(self.URL)
+            URL = tqdm(self.URL.items())
         with requests.Session() as sesh:
-            for line in URL:
+            for caseid, line in URL:
+                ## first make directory
+                path = os.path.join(dir_name, str(caseid))
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                ## next, get HTML data
                 source = sesh.get(line).text
                 soup = BeautifulSoup(source, 'lxml')
                 tr_tags = soup.find_all('tr',  style="page-break-after: always")
+                if self.progress_bar:
+                    tr_tags = tqdm(tr_tags)
                 for tag in tr_tags:
                     tag_list = tag.find_all('tr', class_ = 'label') ## get all tag labels
                     ## get image_id, image type, and part name
@@ -48,7 +56,15 @@ class NASSImageRequest():
                     img = tag.find('img')
                     url_src = img.get('src')
                     img_url =  url_part1 + url_src
-                    #print(img_url)
+                    #print('IMAGE URL', img_url)
+                    #print('Saving to: ', image_name)
                     pull_image = sesh.get(img_url, stream=True)
                     with open(image_name, "wb+") as myfile:
                         myfile.write(pull_image.content)
+
+
+
+
+#https://crashviewer.nhtsa.dot.gov/nass-cds/GetBinary.aspx?Image&ImageID=498459125&CaseID=211017160&Version=1
+#https://crashviewer.nhtsa.dot.gov/nass-cds/GetBinary.aspx?Image&ImageID=498459091&CaseID=211017160&Version=0
+#https://crashviewer.nhtsa.dot.gov/nass-cds/GetBinary.aspx?Image&ImageID=211485731&CaseID=211017982&Version=1
